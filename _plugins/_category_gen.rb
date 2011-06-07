33c8abd61efc84ca3894323c5b9d786ceaae86ac
Charlie Park <charlie@pearbudget.com>
2011-06-07 12:22:57 -0400

    More style, better RSS, CNAME support.

--- _plugins/_category_gen.rb
+++ _plugins/_category_gen.rb
@@ -47,9 +47,9 @@ module Jekyll
       self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
       self.data['tag'] = tag
 
-#      tag_title_prefix = site.config['tag_title_prefix'] || 'Tag: '
-#      self.data['title'] = "#{tag_title_prefix}#{tag}"
-      self.data['title'] = tag
+      tag_title_prefix = site.config['tag_title_prefix'] || 'Posts Tagged &ldquo;'
+      tag_title_suffix = site.config['tag_title_suffix'] || '&rdquo;'
+      self.data['title'] = "#{tag_title_prefix}#{tag}#{tag_title_suffix}"
     end
   end
 
