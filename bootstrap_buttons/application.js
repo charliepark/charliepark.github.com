edcc84abf3ea8e5bbd9b51f24cccb9481a2b52f6
Charlie Park <charlie@pearbudget.com>
2012-02-23 12:37:28 -0500

    Latest build.

--- bootstrap_buttons/application.js
+++ bootstrap_buttons/application.js
@@ -10,8 +10,10 @@ function refreshSwatch() {
 		gradientBottom = "hsl("+hue+", "+saturation+"%, "+lowlight+"%)",
 		borderBottom = "hsl("+hue+", "+saturation+"%, "+superLowlight+"%)",
 		hsl = "hsl("+hue+", "+saturation+"%, "+lightness+"%)",
+		highhex = hsl2Hex(hue, saturation, highlight),
+		lowhex = hsl2Hex(hue, saturation, lowlight),
 		text = getTextColor(lightness, delta),
-		css = generateHSLGradient(hsl, gradientTop, gradientBottom, borderBottom, text),
+		css = generateHSLGradient(hsl, gradientTop, gradientBottom, borderBottom, text, highhex, lowhex),
 		embeddedCss = ".btn-custom {\n"+css+"}";
 		$("button.custom").not('.sample').attr('style', css);
 		$(".ui-slider-range").css("background", hsl);
@@ -37,21 +39,76 @@ function shadowAlpha(puffiness){
 	return a;
 }
 
-function generateHSLGradient(hsl, highlight, lowlight, superLowlight, text) {
+function generateHSLGradient(hsl, highlight, lowlight, superLowlight, text, highhex, lowhex) {
+  // return '  background-color: ' + lowlight + ' !important;\n\
+  //   background-repeat: repeat-x;\n\
+  //   filter: progid:DXImageTransform.Microsoft.gradient(startColorStr="'+highhex+'", endColorStr="'+lowhex+'");\n\
+  //   background-image: -khtml-gradient(linear, left top, left bottom, from('+highlight+'), to('+lowlight+'));\n\
+  //   background-image: -moz-linear-gradient(top, '+highlight+', '+lowlight+');\n\
+  //   background-image: -ms-linear-gradient(top, '+highlight+', '+lowlight+');\n\
+  //   background-image: -webkit-gradient(linear, left top, left bottom, color-stop(0%, '+highlight+'), color-stop(100%, '+lowlight+'));\n\
+  //   background-image: -webkit-linear-gradient(top, '+highlight+', '+lowlight+');\n\
+  //   background-image: -o-linear-gradient(top, '+highlight+', '+lowlight+');\n\
+  //   background-image: linear-gradient('+highlight+', '+lowlight+');\n\
+  //   border-color: '+lowlight+' '+lowlight+' '+superLowlight+';\n\
+  //   '+text+'\n';
 	return '  background-color: ' + lowlight + ' !important;\n\
   background-repeat: repeat-x;\n\
-  background-image: -khtml-gradient(linear, left top, left bottom, from('+highlight+'), to('+lowlight+'));\n\
-  background-image: -moz-linear-gradient(top, '+highlight+', '+lowlight+');\n\
-  background-image: -ms-linear-gradient(top, '+highlight+', '+lowlight+');\n\
-  background-image: -webkit-gradient(linear, left top, left bottom, color-stop(0%, '+highlight+'), color-stop(100%, '+lowlight+'));\n\
-  background-image: -webkit-linear-gradient(top, '+highlight+', '+lowlight+');\n\
-  background-image: -o-linear-gradient(top, '+highlight+', '+lowlight+');\n\
-  background-image: linear-gradient('+highlight+', '+lowlight+');\n\
-  border-color: '+lowlight+' '+lowlight+' '+superLowlight+';\n\
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorStr="'+highhex+'", endColorStr="'+lowhex+'");\n\
+  background-image: -khtml-gradient(linear, left top, left bottom, from('+highhex+'), to('+lowhex+'));\n\
+  background-image: -moz-linear-gradient(top, '+highhex+', '+lowhex+');\n\
+  background-image: -ms-linear-gradient(top, '+highhex+', '+lowhex+');\n\
+  background-image: -webkit-gradient(linear, left top, left bottom, color-stop(0%, '+highhex+'), color-stop(100%, '+lowhex+'));\n\
+  background-image: -webkit-linear-gradient(top, '+highhex+', '+lowhex+');\n\
+  background-image: -o-linear-gradient(top, '+highhex+', '+lowhex+');\n\
+  background-image: linear-gradient('+highhex+', '+lowhex+');\n\
+  border-color: '+lowhex+' '+lowhex+' '+superLowlight+';\n\
   '+text+'\n';
 }
 
-// removed from above code: filter: progid:DXImageTransform.Microsoft.gradient(startColorStr="'+highlight+'", endColorStr="'+lowlight+'");\n\
+function hsl2Hex(h, s, l) {
+  var m1, m2, hue;
+  var r, g, b
+  s /=100;
+  l /= 100;
+  if (s == 0)
+    r = g = b = (l * 255);
+  else {
+    if (l <= 0.5)
+      m2 = l * (s + 1);
+    else
+      m2 = l + s - l * s;
+    m1 = l * 2 - m2;
+    hue = h / 360;
+    r = HueToRgb(m1, m2, hue + 1/3);
+    g = HueToRgb(m1, m2, hue);
+    b = HueToRgb(m1, m2, hue - 1/3);
+  }
+  return "#"+hexify(r) + hexify(g) + hexify(b);
+}
+
+function hexify(i){
+  var hex = parseInt(i).toString(16);
+  if(hex.length == 1){hex="0"+hex};
+  return hex;
+}
+
+function HueToRgb(m1, m2, hue) {
+  var v;
+  if (hue < 0)
+    hue += 1;
+  else if (hue > 1)
+    hue -= 1;
+  if (6 * hue < 1)
+    v = m1 + (m2 - m1) * hue * 6;
+  else if (2 * hue < 1)
+    v = m2;
+  else if (3 * hue < 2)
+    v = m1 + (m2 - m1) * (2/3 - hue) * 6;
+  else
+    v = m1;
+  return 255 * v;
+}
 
 
 $(function() {
