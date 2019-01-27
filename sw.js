026c45ea53d6744d853698ee19e8e0c1bb542ddb
Charlie Park <charlie@ifttt.com>
2019-01-26 23:10:05 -0800

    refactoring service worker js

--- sw.js
+++ sw.js
@@ -1,35 +1,37 @@
 console.log('Service Worker loaded / updated');
 
-const cacheName = '20190126-1106';
+const cacheName = '20190126-2309';
 console.log(cacheName);
 
-self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Install');
+self.addEventListener('install', (event) => {
+  console.log(`[Service Worker] Install`);
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName)
+    .then((cache) => {
       return cache.addAll(
         [
-          'index.html'
+          'index.html',
+          'favicon.ico',
         ]
       );
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-    .then(function(r) {
-      console.log('[Service Worker] Fetching resource: '+event.request.url);
+    .then((r) => {
+      console.log(`[Service Worker] Fetching resource: ${event.request.url}`);
       return r || fetch(event.request)
-        .then(function(response) {
+      .then((response) => {
         return caches.open(cacheName)
-          .then(function(cache) {
-            console.log('[Service Worker] Caching new resource: '+event.request.url);
-            cache.put(event.request, response.clone());
-            return response;
-          });
+        .then((cache) => {
+          console.log(`[Service Worker] Caching new resource: ${event.request.url}`);
+          cache.put(event.request, response.clone());
+          return response;
         });
+      });
     })
   );
 });
